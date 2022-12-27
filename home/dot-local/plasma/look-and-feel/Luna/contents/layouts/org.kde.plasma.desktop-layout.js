3c1ed0c74f31472917175a5e910e2f371354b9e8
klapeto <yiannispana@hotmail.com>
2022-12-27 02:25:50 +0200

    Proper config

---
+++ home/dot-local/plasma/look-and-feel/Luna/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,41 @@
+var panel = new Panel
+var panelScreen = panel.screen
+
+// No need to set panel.location as ShellCorona::addPanel will automatically pick one available edge
+
+// For an Icons-Only Task Manager on the bottom, *3 is too much, *2 is too little
+// Round down to next highest even number since the Panel size widget only displays
+// even numbers
+panel.height = 64
+// panel.height = 2 * Math.floor(gridUnit * 2.5 / 2)
+
+// Restrict horizontal panel to a maximum size of a 21:9 monitor
+const maximumAspectRatio = 21/9;
+if (panel.formFactor === "horizontal") {
+    const geo = screenGeometry(panelScreen);
+    const maximumWidth = Math.ceil(geo.height * maximumAspectRatio);
+
+    if (geo.width > maximumWidth) {
+        panel.alignment = "center";
+        panel.minimumLength = maximumWidth;
+        panel.maximumLength = maximumWidth;
+    }
+}
+
+
+var clock = panel.addWidget("org.kde.plasma.digitalclock")
+clock.currentConfigGroup = ["Appearance"]
+clock.writeConfig("showDate",  false)
+
+var sysTray = panel.addWidget("org.kde.plasma.systemtray")
+sysTray.currentConfigGroup = ["General"]
+sysTray.writeConfig("scaleIconsToFit",  false)
+
+panel.addWidget("org.kde.plasma.pager")
+panel.addWidget("org.kde.plasma.marginsseparator")
+panel.addWidget("org.kde.plasma.taskmanager")
+
+var kickoff = panel.addWidget("org.kde.plasma.kickoff")
+kickoff.currentConfigGroup = ["Shortcuts"]
+kickoff.writeConfig("global", "Alt+F1")
+
