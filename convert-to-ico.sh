b3946c720fd140ee0f46aae5c44689fede84016b
klapeto <yiannispana@hotmail.com>
2023-11-16 01:20:29 +0200

    Icons

--- convert-to-ico.sh
+++ convert-to-ico.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 mkdir raster
 
-SIZES=(256 128 64 48 32 24 22 16)
+SIZES=(512 256 128 96 64 48 32 24 22 16)
 
 FILE=$1
+SUB=$2
 
 for SIZE in ${SIZES[*]}; do
-  inkscape -w ${SIZE} -h ${SIZE} -o ./raster/${SIZE}.png $FILE
+  inkscape -w ${SIZE} -h ${SIZE} -o ./home/dot-local/icons/Luna/${SIZE}x${SIZE}/${SUB}.png $FILE
 done;
 
